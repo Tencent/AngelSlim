0066a0c96d856dc2260f3d807e1009ff39ebe97b
gavingavin99 <266622011+gavingavin99@users.noreply.github.com>
2026-06-09 19:12:08 +0800

    fix(smooth) fix typos and change default smooth config

--- scripts/ptq/run_smooth_for_HY3.sh
+++ scripts/ptq/run_smooth_for_HY3.sh
@@ -33,8 +33,6 @@ done
 # -------- Environment Variables --------
 # Allow function serialization for apply_model in vLLM v1 engine
 export VLLM_ALLOW_INSECURE_SERIALIZATION=1
-# Enable MoE expert statistics collection
-export VLLM_MOE_COLLECT_STATS=1
 # Force Ray to reload code (disable code caching)
 export RAY_DEDUP_LOGS=0
 # Force Python to not use bytecode cache
@@ -43,7 +41,6 @@ export PYTHONDONTWRITEBYTECODE=1
 
 export MAX_NUM_BATCHED_TOKENS=32768
 export VLLM_ENABLE_CHUNKED_PREFILL=1
-export MOE_MODE=fused
 export VLLM_ATTENTION_BACKEND=FLASHINFER
 export ASYNC_SCHEDULING=1
 export VLLM_ENABLE_PREFIX_CACHING=1
@@ -70,6 +67,10 @@ if [ "$SKIP_CONVERT" = false ]; then
     echo "========================================"
     python3 tools/smooth/convert_smooth_weights.py -c "$CONFIG"
 fi
+
+# revert 
+unset VLLM_MOE_COLLECT_SMOOTH_STATS
+unset VLLM_MOE_COLLECT_ALPHA_SEARCH
 
 echo "========================================"
 echo "Done."
